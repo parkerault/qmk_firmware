acbaa600d39f21298910153a680a163933b4e136
Parker Ault <engineering@internetparty.io>
2020-09-30 20:18:32 -0500

    Add leader key to middle .

--- keyboards/xd75/keymaps/parkerault/keymap.c
+++ keyboards/xd75/keymaps/parkerault/keymap.c
@@ -147,10 +147,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_0] = LAYOUT_ortho_5x15(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F10,  KC_F11,  KC_F12,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F7,   KC_F8,   KC_F9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F9,   KC_F8,   KC_F9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   CTRLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, QUO_L5,  \
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F1,   KC_F2,   KC_F3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_CAPS, XXXXXXX, KC_LALT, KC_LGUI, ENT_L1,  EQL_L3,  DF(_6),  XXXXXXX, XXXXXXX, MIN_L4,  SPC_L2,  KC_MEH,  KC_HYPR, KC_MPLY, KC_DEL   \
+  KC_CAPS, XXXXXXX, KC_LALT, KC_LGUI, ENT_L1,  EQL_L3,  DF(_6),  XXXXXXX, KC_BSLS, MIN_L4,  SPC_L2,  KC_MEH,  KC_HYPR, KC_MPLY, KC_DEL   \
   ),
 
   [_1] = LAYOUT_ortho_5x15(
